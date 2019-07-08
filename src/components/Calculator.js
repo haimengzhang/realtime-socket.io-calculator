059d7223bc16b60680a54ecc9d9743d44f8aa0d7
haimengzhang <hzhang1@macalester.edu>
2019-07-08 05:38:11 -0500

    try another fix

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,7 +6,9 @@ import HistoryDisplay from './HistoryDisplay'
 export const CalculatorContext = React.createContext()
 
 const host = "http://127.0.0.1:8000"
-export const socket = socketIOClient(process.env.PORT) //process.env.PORT
+const endPoint = "https://realtime-socketio-calculator.herokuapp.com/"
+const port = process.env.PORT
+export const socket = socketIOClient(endPoint) //process.env.PORT
 
 function Calculator () {
   const [result, setResult] = useState('') // this is the stored calculation result
