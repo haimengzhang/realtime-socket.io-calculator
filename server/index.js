ce846eb8d5aba8f57da5fb06fa08d26a4128bd4c
haimengzhang <hzhang1@macalester.edu>
2019-07-08 05:17:57 -0500

    Fix client connection endpoint

--- server/index.js
+++ server/index.js
@@ -8,13 +8,17 @@ const port = process.env.PORT || 8000
 
 const app = express();
 // app.use(index);
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+
+  app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/../build/index.html');
   });
 
-app.use(express.static(__dirname + '/../build'))
+// app.use(express.static(__dirname + '/../build'))
 
 const server = http.createServer(app); //listen for socket connection
 
