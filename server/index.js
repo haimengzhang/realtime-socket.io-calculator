f02560b01c50a090f20932a1c56ef00b74725bf1
haimengzhang <hzhang1@macalester.edu>
2019-07-09 16:15:36 -0500

    delete comment

--- server/index.js
+++ server/index.js
@@ -6,12 +6,6 @@ const server = http.Server(app)
 const socketIo = require("socket.io");
 
 const port = process.env.PORT || 8000
-// app.use(index);
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
 
 const io = socketIo(server,  {
     handlePreflightRequest: (req, res) => {
